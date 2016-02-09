4080b268c51b09ab2b4cd77d3265ab42bb9e5473
Ryan Breaker <ryan@breaker.rocks>
2016-02-08 23:45:02 -0600

    Rewrite mostly complete, merging after this commit

--- src/csci242/assignments/rsa/Rsa.java
+++ src/csci242/assignments/rsa/Rsa.java
@@ -6,121 +6,99 @@ import org.junit.rules.TemporaryFolder;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 
 /**
  * Project Assignments.
- * Created by breaker on 2/3/16.
+ * Created on 2/3/16.
+ * @author Ryan Breaker ryan@breaker.rocks
  */
 
 
 public class Rsa {
 
     /**
+     * Translates the given char down such that the numeric value of ASCII 'A' is the origin at 0.
      * @param c Character to convert to int.
      * @return The associated int to c.
      */
     private int translateChar(char c) {
-        return c - 'A' + 1;
+        return c - 'A';
     }
 
-//    public void encrypt(FileInOut files, int e, int n) {
-//        String input  = "";
-//        String output = "";
-//
-//        Scanner     fileIn  = files.getInFile();
-//        PrintWriter fileOut = files.getOutFile();
-//
-//        int   arrSize;
-//        int[] cOfI;
-//
-//        while(fileIn.hasNextLine())
-//            input += fileIn.nextLine() + '\n';
-//
-//        arrSize = input.length();
-//
-//        cOfI = new int[arrSize];
-//        for(int i = 0; i < arrSize; i++) {
-//            if(input.charAt(i) == '\n')
-//                cOfI[i] = 0;
-//            else
-//                cOfI[i] = mod(e, n, input.charAt(i));
-//        }
-//
-//        for(int a : cOfI) {
-//            if(a == 0)
-//                output += "0\n";
-//            else
-//                output += String.format("%d\n", a);
-//        }
-//
-//        fileOut.write(output);
-//    }
 
+    /**
+     *
+     * @param files FileInOut object to be used.
+     * @param e First RSA key as an int.
+     * @param n Second RSA key as an int.
+     */
     public void encrypt(FileInOut files, int e, int n) {
-        String input  = "";
-        String output = "";
 
-        int      arrSize;
-        char[][] pairs;
-        int[]    cOfI;
-        int numberOfLines = 0;
-        int[][] indexesOfNewLines;
+        // List of Strings, each representing 1 line of the input file.
+        List<String> input = new ArrayList<String>();
+        // List of collections of Pairs, each collection representing 1 line of the input file.
+        List<Pair[]> pairs = new ArrayList<Pair[]>();
 
+        // Scanner for reading the input file.
         Scanner     fileIn  = files.getInFile();
+        // PrintWriter for writing to the output file.
         PrintWriter fileOut = files.getOutFile();
 
-        while(fileIn.hasNextLine()) {
-            input += fileIn.nextLine();
-            numberOfLines += 1;
+
+        // Grab each line and put it as a new entry in input.
+        while(fileIn.hasNextLine())
+            input.add(fileIn.nextLine());
+
+        // Cleanup input
+        fileIn.close();
+
+
+        // For each line in input, create a new Pair array and add it to `pairs`.
+        for(String line : input) {
+            // Even-out the line's length if currently odd-lengthed by appending 'X'.
+            if(line.length() % 2 != 0)
+                line += 'X';
+
+            // Size of the new Pair array; its even length / 2.
+            int arrSize = line.length() / 2;
+            // The new Pair array, later added to `pairs`.
+            Pair[] newPairArray = new Pair[arrSize];
+
+            // Divide the line into 2-character pieces and create Pairs out of each piece, adding it to the array.
+            for(int i = 0, a = 0; i < line.length() && a < arrSize; i += 2, a++)
+                newPairArray[a] = new Pair(line.charAt(i), line.charAt(i + 1));
+
+            // Finally, add the array to its Collection.
+            pairs.add(newPairArray);
         }
 
-        arrSize = (input.length() / 2;
-        // Make arrSize even if it isn't already
-        if(arrSize % 2 != 0)
-            arrSize += 1;
-
-
-        pairs = new char[arrSize][2];
-        for(int i = 0, a = 0, b = 0; i < input.length() && a < arrSize; i++) {
-            char c = input.charAt(i);
-            if(c == '0') {
-                pairs[a][b] = '0';
-            } else {
-                pairs[a][b] = c;
+        // TODO: Combine this loop into the previous for efficiency.
+        for(Pair[] pairArray : pairs) {
+            for(Pair pair : pairArray) {
+                int c = translateChar(pair.first) * 100 + translateChar(pair.second);
+                fileOut.printf("%d\n", mod(e, n, c));
             }
 
-            // Setup for the next iteration
-            if(i % 2 == 0) {
-                a += 1;
-                b = 1;
-            } else {
-                b = 0;
-            }
+            // Add '0' to indicate a new line in the original file.
+            fileOut.println("0");
         }
 
-        // Even out the number of characters
-        if(pairs[arrSize-1][1] == 0) {
-            pairs[arrSize-1][1] = 'X';
-        }
-
-
-        cOfI = new int[arrSize];
-        for(int i = 0; i < cOfI.length; i++) {
-            if(pairs[i][0] == '0') {
-
-            }
-            cOfI[i] = mod(e, n, translateChar(pairs[i][0]) * 100 + translateChar(pairs[i][1]));
-        }
-
-
-        for(int c : cOfI)
-            output += String.format("%d\n", c);
-
-
-        System.out.println(output);
+        // Close the PrintWriter to flush (write) any new additions to the file.
+        fileOut.close();
     }
 
+
+    /**
+     * Modulation algorithm for easier handling of massive, exponential numbers.
+     * @param e First RSA key
+     * @param n Second RSA key
+     * @param p Pair's numeric ASCII sum (with 'A' subtracted)
+     * @return Returns C = P^e % n
+     */
     private int mod(int e, int n, int p) {
         int result = 1;
         for(int j = 0; j < e; j++)
@@ -128,6 +106,7 @@ public class Rsa {
 
         return result;
     }
+
 
     public static void main(String[] args) throws IOException {
         TemporaryFolder tmp = new TemporaryFolder();
@@ -139,29 +118,35 @@ public class Rsa {
         output = tmp.newFile();
 
         PrintWriter writer = new PrintWriter(input);
-        writer.write("IDESOFMARCH");
+        writer.write("IDES\nOF\nMARCH\n");
         writer.close();
 
         FileInOut fio = new FileInOut(input.getAbsolutePath(), output.getAbsolutePath(), true);
         Rsa rsa = new Rsa();
 
         rsa.encrypt(fio, 17, 2773);
+
+        Scanner scanner = new Scanner(output);
+        while(scanner.hasNextLine())
+            System.out.println(scanner.nextLine());
     }
 
-    public void decrypt(FileInOut files, int d, int n) {
 
+    public void decrypt(FileInOut files, int d, int n) {
         String input = "";
-        while(files.getInFile().hasNextLine())
-            input += files.getInFile().nextLine();
+
+        Scanner fileIn = files.getInFile();
+
+        while(fileIn.hasNextLine())
+            input += fileIn.nextLine();
     }
 }
 
 
-class CharacterPair {
+class Pair {
+    final char first, second;
 
-    public final char first, second;
-
-    CharacterPair(char first, char second) {
+    Pair(char first, char second) {
         this.first = first;
         this.second = second;
     }
---
+++ src/csci242/assignments/rsa/RsaTest.java
@@ -0,0 +1,32 @@
+package csci242.assignments.rsa;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Project Assignments.
+ * Created on 2/8/16.
+ *
+ * @author breaker
+ */
+
+
+public class RsaTest {
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testEncrypt() throws Exception {
+
+    }
+
+    @Test
+    public void testDecrypt() throws Exception {
+
+    }
+}
