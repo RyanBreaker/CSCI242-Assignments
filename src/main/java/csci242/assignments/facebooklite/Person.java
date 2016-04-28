1e788850a40e0b127c4b1b48d9c5c6b773ca48ee
Ryan Breaker <ryan@breaker.rocks>
2016-04-28 13:29:13 -0500

    Add tests for Person

--- src/main/java/csci242/assignments/facebooklite/Person.java
+++ src/main/java/csci242/assignments/facebooklite/Person.java
@@ -1,5 +1,8 @@
 package csci242.assignments.facebooklite;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Short description.
  * <p>
@@ -8,8 +11,30 @@ package csci242.assignments.facebooklite;
  * @author Ryan Breaker
  * @edu.uwp.cs.242.course CSCI242 - Computer Science II
  * @edu.uwp.cs.242.section 001
- * @edu.uwp.cs.242.assignment 2
+ * @edu.uwp.cs.242.assignment 5
  * @bugs None
  */
-public class Person {
+class Person {
+    final String name;
+    private Set<Person> friends = new HashSet<>();
+
+    Person(String name) {
+        this.name = name;
+    }
+
+
+    boolean addFriend(Person person) {
+        //noinspection SimplifiableIfStatement
+        if (person == this || person.friends.contains(this))
+            return false;
+        return friends.add(person);
+    }
+
+    boolean removeFriend(Person person) {
+        return friends.remove(person) || person.friends.remove(this);
+    }
+
+    boolean isFriendsWith(Person person) {
+        return friends.contains(person) || person.friends.contains(this);
+    }
 }
--- src/test/java/csci242/assignments/facebooklite/PersonTest.java
+++ src/test/java/csci242/assignments/facebooklite/PersonTest.java
@@ -1,8 +1,10 @@
 package csci242.assignments.facebooklite;
 
 import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Short description.
@@ -16,9 +18,97 @@ import static org.junit.Assert.*;
  * @bugs None
  */
 public class PersonTest {
-    @Before
-    public void setUp() throws Exception {
+    private Person testPerson;
+    private Person otherPerson;
 
+    /**
+     * Convenience method for adding otherPerson to testPerson.
+     */
+    private void addOtherPersonToTestPerson() {
+        assertTrue(testPerson.addFriend(otherPerson));
     }
 
+    @Before
+    public void setUp() {
+        testPerson = new Person("Bob");
+        otherPerson = new Person("Jane");
+    }
+
+
+    //region addFriend
+    @Test
+    public void testAddFriend() {
+        boolean result = testPerson.addFriend(otherPerson);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testAddFriend_alreadyFriends() {
+        addOtherPersonToTestPerson();
+
+        boolean otherPersonAdd = otherPerson.addFriend(testPerson);
+        boolean testPersonAdd = testPerson.addFriend(otherPerson);
+
+        assertFalse(otherPersonAdd);
+        assertFalse(testPersonAdd);
+    }
+
+    @Test
+    public void testAddFriend_self() {
+        boolean testPersonAddToSelf = testPerson.addFriend(testPerson);
+        assertFalse(testPersonAddToSelf);
+    }
+    //endregion
+
+    //region isFriendsWith
+    @Test
+    public void testIsFriendsWith() {
+        addOtherPersonToTestPerson();
+
+        boolean testPersonIsFriends = testPerson.isFriendsWith(otherPerson);
+        boolean otherPersonIsFriends = otherPerson.isFriendsWith(testPerson);
+
+        assertTrue(testPersonIsFriends);
+        assertTrue(otherPersonIsFriends);
+    }
+
+    @Test
+    public void testIsFriendsWith_not() {
+        boolean testPersonIsFriends = testPerson.isFriendsWith(otherPerson);
+        boolean otherPersonIsFriends = otherPerson.isFriendsWith(testPerson);
+
+        assertFalse(testPersonIsFriends);
+        assertFalse(otherPersonIsFriends);
+    }
+
+    @Test
+    public void testIsFriendsWith_self() {
+        boolean testPersonIsFriendsWithSelf = testPerson.isFriendsWith(testPerson);
+        assertFalse(testPersonIsFriendsWithSelf);
+    }
+    //endregion
+
+    //region removeFriend
+    @Test
+    public void testRemoveFriend() {
+        addOtherPersonToTestPerson();
+
+        boolean removedFromTest = testPerson.removeFriend(otherPerson);
+        boolean removedFromOther = otherPerson.removeFriend(testPerson);
+
+        assertTrue(removedFromTest);
+        assertFalse(removedFromOther);
+    }
+
+    @Test
+    public void testRemoveFriend_fromOther() {
+        addOtherPersonToTestPerson();
+
+        boolean removedFromOther = otherPerson.removeFriend(testPerson);
+        boolean removedFromTest = testPerson.removeFriend(otherPerson);
+
+        assertTrue(removedFromOther);
+        assertFalse(removedFromTest);;
+    }
+    //endregion
 }
